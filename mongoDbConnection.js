c7e3e6a0f953b20f6fc9c62c321b1659635d69e0
Akash gupta <ag244834@gmail.com>
2025-03-02 00:48:47 +0530

    remove some deperceated commands/lines

--- mongoDbConnection.js
+++ mongoDbConnection.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
